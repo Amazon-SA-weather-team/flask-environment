4174e512fa79376c61d117df71f3c1b2b42d29cc
unknown <kirk59435@gmail.com>
2020-06-19 09:55:23 -0400

    flask

---
+++ app/routes.py
@@ -0,0 +1,93 @@
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, login_required, logout_user
+from app import app
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from app import db
+from werkzeug.urls import url_parse
+import requests
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
+def index():
+
+    print(current_user.city)
+    
+    # url = "https://community-open-weather-map.p.rapidapi.com/weather"
+
+    # querystring = {"id":"2172797","units":"%22metric%22 or %22imperial%22","mode":"xml%2C html","q":current_user.city}
+
+    # headers = {
+    #     'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+    #     'x-rapidapi-key': "e0cd5870dcmshdd4c9a6671e756fp12c7f7jsnaa9b5447322b"
+    #     }
+
+    # response = requests.request("GET", url, headers=headers, params=querystring)
+
+    # data = response.json()
+
+    data = {'coord': {'lon': -77.39, 'lat': 38.97}, 
+    'weather': [{'id': 803, 'main': 'Clouds', 'description': 'broken clouds', 'icon': '04d'}], 
+    'base': 'stations', 
+    'main': {'temp': 299.71, 'feels_like': 299.7, 'temp_min': 298.71, 'temp_max': 300.93, 'pressure': 1019, 'humidity': 57}, 
+    'visibility': 16093, 
+    'wind': {'speed': 3.6, 'deg': 160}, 
+    'clouds': {'all': 75}, 
+    'dt': 1592505232, 
+    'sys': {'type': 1, 'id': 4481, 'country': 'US', 'sunrise': 1592473427, 'sunset': 1592527071}, 
+    'timezone': -14400, 
+    'id': 4763793, 
+    'name': 'Herndon', 
+    'cod': 200}
+
+
+    data['main']['temp'] = round(data['main']['temp'] * 9/5 -459.67, 1)
+    data['main']['feels_like'] = round(data['main']['feels_like'] * 9/5 -459.67, 1)
+    data['main']['temp_min'] = round(data['main']['temp_min'] * 9/5 -459.67, 1)
+    data['main']['temp_max'] = round(data['main']['temp_max'] * 9/5 -459.67, 1)
+
+    return render_template("index.html", title='Home Page', data = data)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, address=form.address.data, city=form.city.data)
+        print(user)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+def convertTemp(temp):
+    return temp* 9/5 -459.67
